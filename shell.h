b8c238b4ddd872442c475c414d41476efa16307c
Adora <adoraonyile@gmail.com>
2024-01-12 08:04:10 +0100

    splits a string with strtok

--- shell.h
+++ shell.h
@@ -5,7 +5,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 ssize_t _getline(char **line, size_t *len, FILE *stream);
-
+char **split_str(const char *in, const char *delim, size_t *wc);
 #endif
